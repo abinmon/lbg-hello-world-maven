20e0b3fb4a0ac2babc2315e068cb628e273c8e4c
abinmon <abinmonj@gmail.com>
2023-10-31 10:09:07 +0000

    added my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("AJ"));
         System.out.println(sayGoodbye());
     }
 
